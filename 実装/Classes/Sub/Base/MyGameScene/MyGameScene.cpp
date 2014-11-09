625dd8981bee744187c5ad77d2da2f1c147acf93
星共法 <ulzexus_pc@yahoo.co.jp>
2014-11-09 13:51:36 +0900

    シーンの基底クラスとなるファイルを追加

---
+++ 実装/Classes/Sub/Base/MyGameScene/MyGameScene.cpp
@@ -0,0 +1,13 @@
+//
+//  MyGameScene.cpp
+//  Chess
+//
+//  Created by 星共法 on 2014/11/09.
+//  シーンを管理するクラスの基底クラス
+//  1シーン1レイヤーは崩さない
+
+#include "MyGameScene.h"
+
+namespace MyGame{
+
+}
---
+++ 実装/Classes/Sub/Base/MyGameScene/MyGameScene.h
@@ -0,0 +1,14 @@
+//
+//  MyGameScene.h
+//  Chess
+//
+//  Created by 星共法 on 2014/11/09.
+//
+//
+
+#ifndef __Chess__MyGameScene__
+#define __Chess__MyGameScene__
+
+#include <iostream>
+
+#endif /* defined(__Chess__MyGameScene__) */
